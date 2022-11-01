e877cee4219cb6c20f30d023f7060218a43b6a6b
Frieda Haringsma <fharingsma@hotmail.com>
2022-11-01 21:01:23 +0100

    getYieldForPlant simple function

--- farm.js
+++ farm.js
@@ -1,5 +1,16 @@
 // getYieldForPlant
+const getYieldForPlant = (crops) => {
+  // return yield for plant with no environmental factors
+  return crops.yield;
+};
 
 //getYieldForCrop
+const getYieldForCrop = () => {
+  console.log(crops);
+};
 
 //getTotalYield
+module.exports = {
+  getYieldForPlant,
+  getYieldForCrop,
+};
